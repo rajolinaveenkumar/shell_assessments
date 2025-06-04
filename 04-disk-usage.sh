657cf82fb61a569282b38d522edffe717c423be1
rajolinaveenkumar <rajolinaveenkumar6@gmail.com>
2025-06-04 17:11:25 +0530

    assignment

--- 04-disk-usage.sh
+++ 04-disk-usage.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+UL="\e[4m"
 
 DISK_USAGE=$(df -Th | grep xfs)
 DISK_THRESHOLD=5 
@@ -9,6 +14,7 @@ do
     # echo "Disk usage on $line"
     USE=$(echo $line | awk -F " " '{print $6f}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk -F " " '{print $7f}')
-    echo "Disk Partition is: $PARTITION and Usage is : $USE"
+    echo -e "Disk Partition is: $G $PARTITION $N and Usage is : $G $USE $N"
+    
 
 done <<< $DISK_USAGE
