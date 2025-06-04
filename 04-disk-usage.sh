669868b6fc53dcfbe88ea83d4947fc3d15dedd10
rajolinaveenkumar <rajolinaveenkumar6@gmail.com>
2025-06-04 17:09:43 +0530

    assignment

--- 04-disk-usage.sh
+++ 04-disk-usage.sh
@@ -9,6 +9,6 @@ do
     # echo "Disk usage on $line"
     USE=$(echo $line | awk -F " " '{print $6f}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk -F " " '{print $7f}')
-    echo $PARTITION $USE
+    echo "Disk Partition is: $PARTITION and Usage is : $USE"
 
 done <<< $DISK_USAGE
