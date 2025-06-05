8bfa47a36624e122822f9a4f65b39772d3804c1e
rajolinaveenkumar <rajolinaveenkumar6@gmail.com>
2025-06-05 09:56:40 +0530

    service

---
+++ 05-service-monitor.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SERVICE=$(systemctl status nginx | grep inactive)
+
+if [ $? -eq 0 ]
+then    
+    echo "nginx service is inactive"
+    systemctl start nginx
+else
+    echo "nginx service is active"
+    exit
+fi
