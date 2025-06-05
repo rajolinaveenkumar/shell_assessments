9a63a2f8e15341bb134d1de30e9862c00326fb27
rajolinaveenkumar <rajolinaveenkumar6@gmail.com>
2025-06-05 10:02:08 +0530

    service

--- 05-service-monitor.sh
+++ 05-service-monitor.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "ERROR:: $UL You must have sudo access to execute this script $N"
+    exit 1 # other than 0
+else 
+    echo -e "$G Script name: $0 is executing..... $N"
+fi
 
 SERVICE=$(systemctl status nginx | grep inactive)
 
